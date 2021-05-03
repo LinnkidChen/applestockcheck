238967391a59cc6fef0015a9636f448571eba416
tongchen0510 <linnkid.chen@gmail.com>
2021-05-03 16:32:35 +0800

    update

--- apple_store.py
+++ apple_store.py
@@ -9,31 +9,54 @@ Created on Sun May  2 19:55:14 2021
 from bs4 import BeautifulSoup
 import requests
 import sys
-temp_file=open("temp.txt",'w')
-shop='http://apple.com.cn/shop/buy-'
+temp_file=open("temp1.html",'w')
+# shop='http://apple.com.cn/shop/buy-'
 
-product_type=input("please enter product type.iphone,ipad.etc\n")
-product_type=product_type.replace(' ', '')
-shop=shop+product_type
-product_type_list=['mac','ipad','watch','iphone']
-if (product_type.replace(' ','') not in product_type_list) :
-    print('not a legal product name')
-    sys.exit()
+# product_type=input("please enter product type.iphone,ipad.etc\n")
+# product_type=product_type.replace(' ', '')
+# shop=shop+product_type
+# product_type_list=['mac','ipad','watch','iphone']
+# if (product_type.replace(' ','') not in product_type_list) :
+#     print('not a legal product name')
+#     sys.exit()
 
-#print(shop)
+# #print(shop)
 
-product_name=input("please enter product name.iphone 12,iphone 12 pro etc.\n")
-shop=shop+'/'+product_name.replace(' ','-')
-print(shop)
+# product_name=input("please enter product name.iphone 12,iphone 12 pro etc.\n")
+# shop=shop+'/'+product_name.replace(' ','-')
+# #print(shop)
 
-sourse_shop=requests.get(shop).text
+# sourse_shop=requests.get(shop).text
+sourse_shop=requests.get("https://www.apple.com.cn/shop/buy-iphone/iphone-12").text
 soup_shop=BeautifulSoup(sourse_shop,'lxml')
 
+url_shop=soup_shop.find("meta",property='og:url')
+url_shop_str=url_shop['content']
+url_shop_str=url_shop_str.split('/')
+# print(url_shop_str[-1])
+if(url_shop_str[-1] == '404'):
+    print('Invalid shop address.Please try again')
+    sys.exit()
+#print(url_shop["content"])
 print(soup_shop.title.text)
-#print('invalid shop address.please check input')
-   
+
+reserve_sites=soup_shop.find_all(target="_blank",class_='more')
+print(reserve_sites)
+next_site=None
+for reserve_site in reserve_sites:
+    print(reserve_site.text)
+    if(input("[y] or [n]\n") == 'y'):
+        next_site=reserve_site['href']
+        break;
+
+if(next_site is None):
+    print("Error occours when getting to reservation site.please try again")
+    sys.exit()
+
+
+print (next_site)
     
 
-    
 
-temp_file.write(soup_shop.prettify())
+
+# temp_file.write(soup_shop.prettify())
